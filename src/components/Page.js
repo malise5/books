b6a2a15fa76fa9b79d499e9d94e9b1562733c61c
Halkano Malise <kudezx@gmail.com>
2023-04-01 09:18:22 +0300

    updated

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,22 +1,19 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import Book from "./components/Book";
-import Register from "./components/Register";
 import About from "./components/About";
 
-function App() {
+function Page() {
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/books" element={<Book />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
 }
 
-export default App;
+export default Page;
